7b7d6ad26e7d63e17c72c4a64484004f24919183
lalvaro <lalvaro@betomorrow.com>
2022-08-23 15:10:04 +0200

    all effects in one UI

---
+++ common/CanvasDemoEffect.js
@@ -0,0 +1,16 @@
+class CanvasDemoEffect {
+
+    constructor() {
+        this.mouse = { x: 0, y: 0 };
+        this.textColor = "#888888";
+        this.message = "No message.";
+    }
+
+    init()              {}
+    setSize(w, h)       {}
+    update(time, frame) {}
+    render(renderer)    {}
+    release()           {}
+    onClick(x, y)       {}
+}  
+  
--- demo.js
+++ demo.js
@@ -1,62 +1,67 @@
+
+
+var renderer = null;
+
+
+const changeEffectButton = document.getElementById("changeEffectButton");
+
+changeEffectButton.addEventListener ("click", function() {
+  nextEffect();
+});
+
+var frame = 0;
+var currentEffect = null;
+const canvasEffects = [ new WaterEffect(), new LBMEffect()];
+var effectIndex = -1;
+
+function nextEffect() {
+
+  if(currentEffect) {
+    currentEffect.release();
+  }
+
+  effectIndex = (effectIndex + 1) % canvasEffects.length;
+
+  currentEffect = canvasEffects[effectIndex];
+  currentEffect.init();
+
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  currentEffect.setSize(width, height);
+
+  document.getElementById("title").style.color = currentEffect.textColor;  
+  document.getElementById("message").style.color = currentEffect.textColor;    
+  document.getElementById("message").innerHTML = currentEffect.message;
+
+  frame = 0;
+}
+
 function main() {
 
-  var iFrame = 0;
-
-  const renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer();
   document.body.appendChild( renderer.domElement );  
 
-  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
-  const main_scene = new THREE.Scene();
-  const lay1_scene = new THREE.Scene();
-  const lay2_scene = new THREE.Scene();  
+  nextEffect();
 
-  const renderTarget_options = { format : THREE.RGBAFormat, type : THREE.FloatType, depthBuffer : false }
-
-  const renderTarget1 = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, renderTarget_options);
-  const renderTarget2 = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, renderTarget_options);
-
-  const main_uniforms = {
-    iTime: { value: 0 },
-    iResolution:  { value: new THREE.Vector3() },
-    layer2 : { value : renderTarget2.texture },
-  };
-  const main_material = new THREE.ShaderMaterial({
-    fragmentShader : main_fragmentShader,
-    uniforms : main_uniforms,
-    glslVersion: THREE.GLSL3, 
-  });
+  document.onmousemove = handleMouseMove;
   
-  const layer1_uniforms = {
-    iTime: { value: 0 },
-    iResolution:  { value: new THREE.Vector3() },
-    layer2 : { value : renderTarget2.texture },
-  };
-  const layer1_material = new THREE.ShaderMaterial({
-    fragmentShader : stream_fragmentShader,
-    uniforms : layer1_uniforms,
-    glslVersion: THREE.GLSL3,     
-  });
+  function handleMouseMove(event) {
+      event = event || window.event; 
 
-  const layer2_uniforms = {
-    iTime: { value: 0 },
-    iResolution:  { value: new THREE.Vector3() },
-    iMouse:  { value: new THREE.Vector4() },
-    layer1 : { value : renderTarget1.texture },
-    iFrame : { value : 0 },    
-  };
-  const layer2_material = new THREE.ShaderMaterial({
-    fragmentShader : collide_fragmentShader,
-    uniforms : layer2_uniforms,
-    glslVersion: THREE.GLSL3,     
-  });
+      currentEffect.mouse.x = event.pageX;
+      currentEffect.mouse.y = event.pageY;      
+  }
 
-  const plane = new THREE.PlaneGeometry(2, 2);
+  function onClick(event) {
+    currentEffect.onClick(event.pageX, event.pageY);
+  }
 
-  //main_scene.add(new THREE.Mesh(plane, main_material));
-  lay1_scene.add(new THREE.Mesh(plane, layer1_material));
-  lay2_scene.add(new THREE.Mesh(plane, layer2_material));
-  main_scene.add(new THREE.Mesh(plane, main_material));
+  document.addEventListener("click", onClick);
+
   
+   
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
@@ -65,59 +70,27 @@ function main() {
 
     if (needResize) {
       renderer.setSize(width, height, false);
-      renderTarget1.setSize(width, height, false);
-      renderTarget2.setSize(width, height, false);
-
-      main_uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
-      layer1_uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
-      layer2_uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
-
-      iFrame = 0;
+      currentEffect.setSize(width, height);
+      frame = 0;
     }
     return needResize;
   }
 
   function render(time) {
 
-    time *= 0.001; 
-
     resizeRendererToDisplaySize(renderer);
 
-    const canvas = renderer.domElement;
-    main_uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
-    layer1_uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
-    layer2_uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
-
-    main_uniforms.iTime.value   = time;
-    layer1_uniforms.iTime.value = time;
-    layer2_uniforms.iTime.value = time;        
-
-    layer2_uniforms.iFrame.value = iFrame++;    
-
-    layer2_uniforms.iMouse.value = new THREE.Vector4(mousePos.x, canvas.height-mousePos.y, 1, 1);
-if(iFrame<200000) {
-    renderer.setRenderTarget(renderTarget2);
-    renderer.render(lay2_scene, camera);
-
-    renderer.setRenderTarget(renderTarget1);
-    renderer.render(lay1_scene, camera);
-}
-    renderer.setRenderTarget(null);
-    renderer.render(main_scene, camera); 
+    currentEffect.update(time*0.001, frame);
+    currentEffect.render(renderer);
     
+    frame++;
+
     requestAnimationFrame(render);
   }
 
   requestAnimationFrame(render);
 }
 
-var mousePos = { x: 0, y: 0};
-document.onmousemove = handleMouseMove;
-
-function handleMouseMove(event) {
-    event = event || window.event; 
-    mousePos = { x: event.pageX, y: event.pageY };
-}
 
 
 main();
