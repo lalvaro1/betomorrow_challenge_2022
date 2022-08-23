cf6ce867040a87ddbaf67af33c1912a9b5900287
lalvaro <lalvaro@betomorrow.com>
2022-08-23 17:40:35 +0200

    files reorganized

---
+++ src/common/CanvasDemoEffect.js
@@ -0,0 +1,16 @@
+class CanvasDemoEffect {
+
+    constructor() {
+        this.mouse = { x: -9999, y: -9999 };
+        this.textColor = "#888888";
+        this.message = "No message.";
+    }
+
+    init()              {}
+    setSize(w, h)       {}
+    update(time, frame) {}
+    render(renderer)    {}
+    release()           {}
+    onClick(x, y)       {}
+}  
+  
